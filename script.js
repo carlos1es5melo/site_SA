eccb86d5c65170814ce6fbdf1944d386d53a8f16
CARLOS EDUARDO SOUZA MELO <carlos_es_melo@estudante.sesisenai.org.br>
2026-03-25 16:35:46 -0300

    terceira versão

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ function calcularF(f) {
     return (f - 32) / 1.8;
 }
 
+let resposta = document.getElementById('resposta')
+
 function principal() {
-    let op = document.getElementById('conversor')
-    let temp = document.getElementById('temperatura')
+    let op = document.getElementById('conversor').value
+    let temperatura = Number(document.getElementById('temperatura').value)
 
     if (op === 'F') {
-        let fahr = calcularF(temp);
-        console.log("Resultado:", fahr);
+        let fahr = calcularC(temperatura)
+        resposta.innerHTML = `Resultado = ${fahr.toFixed(2)} °F`
     } else if (op === 'C') {
-        let celsius = calcularC(temp);
-        console.log("Resultado:", celsius);
+        let celsius = calcularF(temperatura)
+        resposta.innerHTML = `Resultado = ${celsius.toFixed(2)} °C`
     }
 }
-
-principal();
